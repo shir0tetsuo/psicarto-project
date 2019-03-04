0ae01d8c7534d73d1aa45f8c6f99482888bdde87
shir0tetsuo <shadowsword@protonmail.com>
2019-03-03 20:29:27 -0500

    Trying to see login request in console terminal

--- server.js
+++ server.js
@@ -8,7 +8,10 @@ const path = require('path')
 const Discord = require ("discord.js"); // discord client
 const client = new Discord.Client(); // discord client
 
-function CreateNavigator() {
+
+
+function CreateNavigator(request, response) {
+  // Read token header from client-end here
   return fs.readFileSync('pages/index.html')
 }
 
@@ -16,7 +19,7 @@ sys.use(express.static('public'))
 
 sys.get('/', (request, response) => {
   const fsHEAD = fs.readFileSync('pages/head.html')
-  const fsAAA = CreateNavigator()// Do some extra stuff to ensure login here
+  const fsAAA = CreateNavigator(request, response)// Do some extra stuff to ensure login here
   const fsTRAIL = fs.readFileSync('pages/trail.html')
 
   const GumGum = fsHEAD + fsAAA + fsTRAIL
@@ -24,8 +27,21 @@ sys.get('/', (request, response) => {
   response.send(GumGum)
 })
 
+sys.post('/login', (request, response) => {
+  console.log(request)
+})
+
+
+client.on("ready", () => {
+  console.log("SPIRITO-BOT ONLINE")
+  client.user.setStatus("online")
+});
+
 sys.listen(2600, () => {
   console.log('App listening.')
+  client.login(settings.token);
+  console.log('Discord listening.')
+  client.on('error', console.error)
 })
 
 /////////////////////////////////////////////////////////
