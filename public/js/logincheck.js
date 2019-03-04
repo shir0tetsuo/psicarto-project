6a0d020b8f5bb3c557fa32aa7ec47c19e4a78502
shir0tetsuo <shadowsword@protonmail.com>
2019-03-04 03:23:05 -0500

    number checking mechanism

--- public/js/logincheck.js
+++ public/js/logincheck.js
@@ -4,6 +4,7 @@ function check_user() {
   flashBox = document.getElementById("flashArray"),
   flashMSG = document.getElementById("flashComm"),
   boxCTL = document.getElementById("buttonControl")
+  const digits_only = string => [...string].every(c => '0123456789'.includes(c))
   console.log("Update issued")
   if (idnum.value.length !== 0 && key.value.length !== 0) {
     flashBox.style.display = "block"
@@ -14,7 +15,13 @@ function check_user() {
     flashMSG.innerHTML = "Please enter a Discord ID or Auth Key<br>Your Auth Key should be a unique ID submitted to you via DMs"
     boxCTL.style.display = "none"
   } else {
-    flashBox.style.display = "none"
-    boxCTL.style.display = "block"
+    if (digits_only(idnum.value) == true) {
+      flashBox.style.display = "none"
+      boxCTL.style.display = "block"
+    } else {
+      flashBox.style.display = "block"
+      flashMSG.innerHTML = "Your Discord ID should only be numbers."
+      boxCTL.style.display = "none"
+    }
   }
 }
