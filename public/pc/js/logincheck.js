82b8d14f2d5b826fb497e248f00b692bd10e56ea
shir0tetsuo <shadowsword@protonmail.com>
2019-03-05 17:14:45 -0500

    bad idea

--- public/pc/js/logincheck.js
+++ public/pc/js/logincheck.js
@@ -3,18 +3,18 @@ function check_user() {
   key = document.getElementById("key"),
   flashBox = document.getElementById("flashArray"),
   flashMSG = document.getElementById("flashComm"),
-  //boxCTL = document.getElementById("buttonControl")
+  boxCTL = document.getElementById("buttonControl")
   console.log("Update issued")
   if (idnum.value.length !== 0 && key.value.length !== 0) {
     flashBox.style.display = "block"
     flashMSG.innerHTML = "Both fields cannot be filled at the same time"
-    //boxCTL.style.display = "none"
+    boxCTL.style.display = "none"
   } else if (idnum.value.length == 0 && key.value.length == 0) {
     flashBox.style.display = "block"
     flashMSG.innerHTML = "Please enter a Discord ID or Auth Key<br>Your Auth Key should be a unique ID submitted to you via DMs"
-    //boxCTL.style.display = "none"
+    boxCTL.style.display = "none"
   } else {
     flashBox.style.display = "none"
-    //boxCTL.style.display = "block"
+    boxCTL.style.display = "block"
   }
 }
