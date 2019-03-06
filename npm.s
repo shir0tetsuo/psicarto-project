ba02372290981318e12d4758f756409195b48a77
shir0tetsuo <shadowsword@protonmail.com>
2019-03-06 01:20:49 -0500

    Installed dependencies

--- npm.s
+++ npm.s
@@ -44,6 +44,7 @@ npm install --save-dev firebase
 npm install --save firebase-admin
 #npm install --save sqlite3 #database
 #npm install --save chess.js
+npm install grpc
 #cp /node/BAK/node_modules/sqlite -r /root/NC/utils/NorthStar/sabre.discord.js/node_modules/
 echo Done
 
