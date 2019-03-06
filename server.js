90a2f405c1da292a3c44fc1831d53886dd2f0271
shir0tetsuo <shadowsword@protonmail.com>
2019-03-06 00:18:39 -0500

    mistake

--- server.js
+++ server.js
@@ -21,7 +21,8 @@ var axis = new Object();
 var message = {text: 'Hello, World!', timestamp: new Date().toString()};
 var ref = firebase.database().ref('/pc-user');
 var logRef = ref.child('logs')
-var messagesRef = messagesRef.push(message)
+var messagesRef = ref.child('messages')
+var messageRef = messagesRef.push(message)
 ref.once('value').then(function(snap) {
   console.log('snap.val()', snap.val())
 })
