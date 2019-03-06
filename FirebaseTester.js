721357896ed285878e57da14eb062d9b3ee28541
shir0tetsuo <shadowsword@protonmail.com>
2019-03-06 00:56:51 -0500

    modified path

--- FirebaseTester.js
+++ FirebaseTester.js
@@ -1,6 +1,6 @@
 const admin = require('firebase-admin');
 
-var serviceAccount = require('service-account.json');
+var serviceAccount = require('./service-account.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
