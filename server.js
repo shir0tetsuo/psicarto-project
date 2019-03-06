411a6bb0536e6388279bd1ba43e2b35b7d7c0453
shir0tetsuo <shadowsword@protonmail.com>
2019-03-05 19:14:33 -0500

    test

--- server.js
+++ server.js
@@ -124,8 +124,7 @@ sys.get('/pc', (request, response) => {
   const GumGum = fsHEAD + fsAAA + fsTRAIL
   //response.sendFile(path.resolve(__dirname, 'pages/index.html'))
   response.send(GumGum)
-}
-catch(){
+}).catch(){
   response.send('Error')
 })
 // TODO: Test idnumber against key, add database
