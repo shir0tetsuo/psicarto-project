9a1b9bc86048838153de198792d5b8f4591bacc7
shir0tetsuo <shadowsword@protonmail.com>
2019-03-06 10:24:21 -0500

    null obj cannot be detected

--- server.js
+++ server.js
@@ -119,6 +119,7 @@ sys.get('/pc/base', (request, response) => {
   // Connect to database here. Use cookie parser here.
   // if (cookie[database] !== undefined) .. else { response.send() }
   console.log(request.cookies)
+  ////////// BROKEN: Null object cannot be detected?
   if (request.cookies == null) {
     response.redirect("/pc")
     return
