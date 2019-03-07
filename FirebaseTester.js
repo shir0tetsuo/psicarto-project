5df653df6a42b8f918dd53fe083cb374ed17d88d
shir0tetsuo <shadowsword@protonmail.com>
2019-03-06 21:29:03 -0500

    test

--- FirebaseTester.js
+++ FirebaseTester.js
@@ -22,8 +22,7 @@ console.log('20')
 db.collection('pc-user').get()
   .then((snapshot) => {
     snapshot.forEach((doc) => {
-      var docDataVal = doc.data();
-      var docSlice = docDataVal.slice()
+      var docDataVal = doc.data().filter(d => d.key);
       console.log(docDataVal)
       console.log(doc.id, '=>', doc.data());
     });
